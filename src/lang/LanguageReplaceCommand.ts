913cd5364953c1b2556aeedda75bcc6b0c3d2533
javarome <javarome@gmail.com>
2025-05-17 20:47:48 +0200

    fix file lang variants

--- src/lang/LanguageReplaceCommand.ts
+++ src/lang/LanguageReplaceCommand.ts
@@ -11,33 +11,36 @@ export class LanguageReplaceCommand extends DomReplaceCommand<HTMLElement, HtmlR
   }
 
   protected async createReplacer(context: HtmlRR0Context): Promise<DomReplacer> {
-    const doc = context.file.document
     return new class implements DomReplacer {
       async replace(original: HTMLElement): Promise<HTMLElement> {
         if (!original.hasChildNodes()) {
           const inputFile = context.file
+          const fileName = inputFile.name
           const langInfo = inputFile.lang
           const variants = langInfo.variants
-          const foundLang = langInfo.lang
+          let fileLang = langInfo.lang
           let pageLang: string
           const hasEnglishVariant = variants.includes("en")
           if (hasEnglishVariant) {
-            pageLang = foundLang ? foundLang : "fr"
-          } else {
-            if (variants.includes("fr")) {
-              pageLang = foundLang && foundLang !== "fr" ? foundLang : "en"
+            pageLang = fileLang ? fileLang : "fr"
+          } else if (variants.includes("fr")) {
+            if (fileLang && fileLang !== "fr") {
+              pageLang = fileLang
             } else {
-              pageLang = "fr"
+              pageLang = "en"
+              fileLang = fileName.includes("_" + pageLang) ? pageLang : ""
             }
+          } else {
+            pageLang = "fr"
           }
-          context.file.document.documentElement.lang = inputFile.lang.lang = pageLang
+          inputFile.document.documentElement.lang = inputFile.lang.lang = pageLang
           const langVariants = variants.length == 1 && variants[0] == "" ? [pageLang == "fr" ? "en" : "fr"] : variants
-          const fileName = inputFile.name
+          const doc = inputFile.document
           for (let i = 0; i < langVariants.length; i++) {
             const langVariant = langVariants[i]
             const altLink = doc.createElement("a")
-            const toReplace = (foundLang ? "_" + foundLang : "") + "."
-            const replacement = `${variants[i] == "" ? "" : "_" + langVariant}.`
+            const toReplace = (fileLang ? "_" + fileLang : "") + "."
+            let replacement = `${variants[i] == "" ? "" : "_" + langVariant}.`
             altLink.href = "/" + fileName.replace(toReplace, replacement)
             const altText = langVariant === "en" ? "English version" : "Version française"
             altLink.textContent = Buffer.from(altText, "utf-8").toString()
