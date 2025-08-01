09802b6baee9bfaf601ac4b61218f777a17e5335
javarome <javarome@gmail.com>
2025-08-02 01:07:50 +0200

    docs

--- src/people/PeopleReplacer.test.ts
+++ src/people/PeopleReplacer.test.ts
@@ -10,7 +10,9 @@ describe("PeopleReplacer", () => {
 
   const rootDir = rr0TestUtil.filePath("people")
   const files = [
+    path.join(rootDir, "j/Jésus"),
     path.join(rootDir, "b/BeauJerome"),
+    path.join(rootDir, "j/Jesus"),
     path.join(rootDir, "h/HynekJosefAllen"),
     path.join(rootDir, "r/ReaganRonald"),
     path.join(rootDir, "v/VertongenJeanLuc")
@@ -49,35 +51,41 @@ describe("PeopleReplacer", () => {
     }
   })
 
-  test("replace people tags", {skip: true}, async () => {
+  test("replace people tags", async () => {
     const dataService = new AllDataService([peopleFactory])
     const peopleService = new PeopleService(dataService, peopleFactory, {rootDir, files})
     const peopleRenderer = new PeopleHtmlRenderer()
     const replacer = new PeopleReplacer(peopleService, peopleRenderer)
     const context = rr0TestUtil.time.newHtmlContext("1/9/9/0/08/index.html", "")
     {
-      const peopleWithTitle = createPeopleElement(context, "Jean-Luc Vertongen")
+      const peopleWithTitle = createPeopleElement(context, "Jésus")
       const replacement = await replacer.replacement(context, peopleWithTitle)
       expect(replacement.outerHTML).toBe(
-        `<span class="peopl" translate="no"><a href="/src/people/v/VertongenJeanLuc/">Jean-Luc Vertongen</a></span>`)
+        `<span class="data-resolved people-resolved" translate="no"><a href="/test/people/j/Jesus/">Jésus</a></span>`)
     }
     {
-      const peopleWithTitle = createPeopleElement(context, "Ronald Reagan", "Ronald Wilson Reagan")
+      const peopleWithComposedFirstName = createPeopleElement(context, "Jean-Luc Vertongen")
+      const replacement = await replacer.replacement(context, peopleWithComposedFirstName)
+      expect(replacement.outerHTML).toBe(
+        `<span title="1939-2015, 76 ans, Belgique, ufologue" class="data-resolved people-resolved deceased country-be occupation-ufologist" translate="no"><a href="/test/people/v/VertongenJeanLuc/">Jean-Luc Vertongen<img src="/test/people/v/VertongenJeanLuc/portrait.jpg" alt="Jean-Luc Vertongen" class="portrait" width="75"></a></span>`)
+    }
+    {
+      const peopleWithTitle = createPeopleElement(context, "Reagan", "Ronald Reagan")
       const replacement = await replacer.replacement(context, peopleWithTitle)
       expect(replacement.outerHTML).toBe(
-        `<span class="peopl" translate="no"><a href="/src/people/r/ReaganRonald/">Ronald Reagan</a></span>`)
+        `<span title="1911-2004, 93 ans, USA, acteur, politicien, dirigeant" class="data-resolved people-resolved deceased country-us occupation-actor occupation-politician occupation-leader" translate="no"><a href="/test/people/r/ReaganRonald/">Reagan<img src="/test/people/r/ReaganRonald/portrait.gif" alt="Reagan" class="portrait" width="75"></a></span>`)
     }
     {
       const peopleWithFullName = createPeopleElement(context, "Jérôme Beau")
       let replacement = await replacer.replacement(context, peopleWithFullName)
       expect(replacement.outerHTML).toBe(
-        `<span title="1972-, 50 ans, France, ufologue, Informaticien" class="country-fr occupation-ufologist occupation-softwareEngineer" translate="no"><a href="/src/people/b/BeauJerome/">Jérôme Beau</a></span>`)
+        `<span class="data-resolved people-resolved" translate="no"><a href="/test/people/b/BeauJerome/">Jérôme Beau</a></span>`)
     }
     {
       const peopleWithLastName = createPeopleElement(context, "Beau")
       let replacement = await replacer.replacement(context, peopleWithLastName)
       expect(replacement.outerHTML).toBe(
-        `<span title="1972-, 50 ans, France, ufologue, Informaticien" class="country-fr occupation-ufologist occupation-softwareEngineer" translate="no" title="Jérôme Beau"><a href="/src/people/b/BeauJerome/" title="Jérôme Beau">Beau</a></span>`)
+        `<span class="data-resolved people-resolved" translate="no"><a href="/test/people/b/BeauJerome/">Beau</a></span>`)
     }
   })
 })
